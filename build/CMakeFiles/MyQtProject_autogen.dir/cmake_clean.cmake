e7e3fc0675b9a58431ea3f2b2737e45414d6d6e7
Quebec-Eric <quebec.ericjs@gmail.com>
2023-08-10 12:51:25 -0300

    Trabalho1

---
+++ build/CMakeFiles/MyQtProject_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyQtProject_autogen"
+  "MyQtProject_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MyQtProject_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MyQtProject_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for MyQtProject_autogen.
+# This may be replaced when dependencies are built.
